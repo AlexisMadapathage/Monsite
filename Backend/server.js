49ea6429d9fdbb84e08c7620985a53f97983a685
Alexis Madapathage <alexis.madasen@outlook.fr>
2025-05-01 15:10:31 +0200

    Port modifié

--- Backend/server.js
+++ Backend/server.js
@@ -31,7 +31,7 @@ app.use("/api/contact", contactRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connecté à MongoDB");
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT;
     console.log("PORT utilisé :", PORT); // debug
 
     app.listen(PORT, () => {
